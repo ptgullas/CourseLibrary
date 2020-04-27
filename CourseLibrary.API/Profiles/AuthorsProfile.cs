5e9b4ca9beccfc5d91ba42e503dc38a084210d07
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-04-26 22:34:07 -0400

    Add CreateMap for AuthorForCreationDto to Author

--- CourseLibrary.API/Profiles/AuthorsProfile.cs
+++ CourseLibrary.API/Profiles/AuthorsProfile.cs
@@ -13,6 +13,8 @@ namespace CourseLibrary.API.Profiles {
                 opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
                 .ForMember(dest => dest.Age,
                 opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+
+            CreateMap<Models.AuthorForCreationDto, Entities.Author>();
         }
     }
 }
