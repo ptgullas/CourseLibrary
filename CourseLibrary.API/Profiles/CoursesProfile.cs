655be6188c76125390968cab591ce78daf31dd52
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-04-26 23:03:16 -0400

    Map CourseForCreationDto to Course entity

--- CourseLibrary.API/Profiles/CoursesProfile.cs
+++ CourseLibrary.API/Profiles/CoursesProfile.cs
@@ -8,6 +8,7 @@ namespace CourseLibrary.API.Profiles {
     public class CoursesProfile : Profile {
         public CoursesProfile() {
             CreateMap<Entities.Course, Models.CourseDto>();
+            CreateMap<Models.CourseForCreationDto, Entities.Course>();
         }
     }
 }
