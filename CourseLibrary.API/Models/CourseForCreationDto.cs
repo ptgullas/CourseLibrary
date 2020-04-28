71e916c50f108f0683e5a1824ca0addcbd5fa4f9
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-04-28 18:50:24 -0400

    added error messages to Required & MaxLength attributes

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 namespace CourseLibrary.API.Models {
     [CourseTitleMustBeDifferentFromDescription]
     public class CourseForCreationDto /* : IValidatableObject */ {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
 
-        [MaxLength(1500)]
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public string Description { get; set; }
 
         //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
