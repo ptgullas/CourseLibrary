c92dedca9db54f48de0f4bbcd561d814bb2028ac
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-04-28 18:45:52 -0400

    Applied CourseTitleMustBeDifferentFromDescription

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models {
-    public class CourseForCreationDto : IValidatableObject {
+    [CourseTitleMustBeDifferentFromDescription]
+    public class CourseForCreationDto /* : IValidatableObject */ {
         [Required]
         [MaxLength(100)]
         public string Title { get; set; }
@@ -13,12 +15,12 @@ namespace CourseLibrary.API.Models {
         [MaxLength(1500)]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
-            if (Title == Description) {
-                yield return new ValidationResult(
-                    "The provided description should be different from the title.",
-                    new[] { "CourseForCreationDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
+        //    if (Title == Description) {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
