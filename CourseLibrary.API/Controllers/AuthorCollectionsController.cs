06ad2e12ba466617aefa27ac481c5fdb2c586349
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-04-28 13:50:15 -0400

    + AuthorCollectionsController

---
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers {
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository,
+    IMapper mapper) {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+    }
+}
