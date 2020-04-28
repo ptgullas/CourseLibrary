ac294b4a1bbee702fd77119ba2f9e6c4ad926fe7
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-04-28 18:09:39 -0400

    Added validation annotations

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models {
     public class CourseForCreationDto {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [MaxLength(1500)]
         public string Description { get; set; }
     }
 }
