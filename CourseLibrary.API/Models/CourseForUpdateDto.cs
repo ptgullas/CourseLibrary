ddcc897c4ad94bbaa46177cb73b4d7fd5d133592
ptgullas <4305689+ptgullas@users.noreply.github.com>
2020-04-28 22:03:25 -0400

    override Description w/Required attribute

--- CourseLibrary.API/Models/CourseForUpdateDto.cs
+++ CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -7,5 +7,8 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models {
     public class CourseForUpdateDto : CourseForManipulationDto {
+        [Required(ErrorMessage = "You should fill out a description.")]
+        public override string Description { get => base.Description; set => base.Description = value; }
+
     }
 }
